e5910c8942fa354fa5ead5b1bb1d3c0c472f8679
L. Bland <lb831@eng.cam.ac.uk>
2025-04-24 21:27:51 +0100

    small edits

---
+++ InputFiles/output.m
@@ -0,0 +1,18 @@
+seed = -1013309985;
+pop = 15000;
+Inactive_Cycles = 200;
+inactive_particles_per_s = Infinity;
+Active_Cycles = 500;
+active_particles_per_s = Infinity;
+Total_CPU_Time = 2.13767E+02;
+Total_Transport_Time = 1.23948E+01;
+inactive_batchSize = 1;
+batchSize = 1;
+keff_IMP_PROD = [ 1.50112E+04,8.01338E+00];
+keff_IMP_ABS = [ 9.21243E+03,4.84131E+00];
+keff_SCATTER_PROD = [ 0.00000E+00,0.00000E+00];
+keff_ANA_LEAK = [ 5.78719E+03,2.94916E+00];
+keff_K_EFF = [ 1.00075E+00,3.57251E-04];
+active_batchSize = 1;
+active_fiss_Res = reshape([ 1.00000E+02,0.00000E+00],2,1);
+
